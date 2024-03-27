0bafe54e9a0ba7eb43001d1b63f1839ac3c3cbbd
bipinlongjam <bipinlongjam11@gmail.com>
2024-03-27 23:49:14 +0530

    req and response

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const server = http.createServer((req, res) =>{
     res.setHeader('Content-Type', 'text/plain');
    
     console.log("My name is Bipin");
-    res.end('My name is Bipin\n');
-    // process.exit();
+    //res.end('My name is Bipin\n');
+    console.log("Welcome to my Node.js project");
+   // res.end('Welcome to my Node.js project\n');
+   if (req.url === '/home') {
+    console.log("Welcome home");
+    res.write('Welcome home\n');
+} else if (req.url === '/about') {
+    console.log("Welcome to About Us page");
+    res.write('Welcome to About Us page\n');
+} else if (req.url === '/node') {
+    console.log("Welcome to my Node Js project");
+    res.write('Welcome to my Node Js project\n');
+} else {
+    console.log("Welcome to my Node Js project");
+    res.write('Welcome to my Node Js project\n');
+}
+
+res.end(); 
 })
 server.listen(4000);
